6c49aef5917c1a14c1a8e69364fb82ca2e24ce89
karohoo <karoliina.hautala@myy.haaga-helia.fi>
2019-02-05 19:04:55 +0200

    First commit

---
+++ src/main/java/hh/palvelinohjelmointi/Bookstore/web/BookController.java
@@ -0,0 +1,14 @@
+package hh.palvelinohjelmointi.Bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookController {
+
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String mainPage() {
+		return "mainpage";
+	}
+}
