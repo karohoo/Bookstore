dacb0cc65af64b6a8b229acc6087bf54e467de71
karohoo <karoliina.hautala@myy.haaga-helia.fi>
2019-03-12 09:48:43 +0200

    Modifying login

--- src/main/java/hh/palvelinohjelmointi/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/palvelinohjelmointi/Bookstore/BookstoreApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import hh.palvelinohjelmointi.Bookstore.domain.BookRepository;
 import hh.palvelinohjelmointi.Bookstore.domain.Category;
 import hh.palvelinohjelmointi.Bookstore.domain.CategoryRepository;
+import hh.palvelinohjelmointi.Bookstore.domain.User;
+import hh.palvelinohjelmointi.Bookstore.domain.UserRepository;
 import hh.palvelinohjelmointi.Bookstore.domain.Book;
 
 @SpringBootApplication
@@ -21,7 +23,8 @@ public class BookstoreApplication {
 	}
 	
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository bookrepository, CategoryRepository categoryrepository) {
+	public CommandLineRunner bookDemo(BookRepository bookrepository, CategoryRepository 
+			categoryrepository, UserRepository userrepository) {
 		return (args) -> {
 			log.info("saving books");
 			categoryrepository.save(new Category("Fantasy"));
@@ -31,6 +34,10 @@ public class BookstoreApplication {
 					1997, "2545-45646496", 15.99, categoryrepository.findByName("Fantasy").get(0)));
 			bookrepository.save(new Book("Harry Potter and the Deathly Hallows", "J. K. Rowling", 
 					2009, "3564970-65465468", 19.99, categoryrepository.findByName("Fantasy").get(0)));
+			userrepository.save(new User("user", "$2a$04$zLnuuuCimyp8lKg/iLQsPeHOvn5SFG.XfYPlcE3zjXpKDm14gEZNW",
+					"user@user.com", "USER"));
+			userrepository.save(new User("admin", "$2a$04$nnSBGiNk6ugfSX647Tw5ruvtx1m9RgcNpNtVjo25q.kjKhYmv/Jgm",
+					"admin@admin.com", "ADMIN"));
 			log.info("fetch all books");
 			for (Book book : bookrepository.findAll()) {
 				log.info(book.toString());
